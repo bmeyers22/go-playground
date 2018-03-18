6c9a4967d72f1f5eaef4bd3fffb9c14cbeef10ee
Brian Meyers <meyers.briant@gmail.com>
2018-03-18 13:26:05 -0400

    Tryihng with main var

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/bmeyers22/go-playground/plugin"
+import (
+	"github.com/bmeyers22/go-playground/plugin"
+	"fmt"
+)
+
+var (
+	MainString = "test"
+)
 
 func main() {
+	fmt.Printf("Main var: %s\n", MainString)
 	plugin.PrintTestVar()
 }
